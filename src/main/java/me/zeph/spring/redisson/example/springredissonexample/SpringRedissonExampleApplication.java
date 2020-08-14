a4856ac8501028c7b23dd8b04f69d40450965ab1
benweizhu <benweizhu@gmail.com>
2020-08-14 10:43:09 +0800

    init

---
+++ src/main/java/me/zeph/spring/redisson/example/springredissonexample/SpringRedissonExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.redisson.example.springredissonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedissonExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedissonExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/zeph/spring/redisson/example/springredissonexample/SpringRedissonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.redisson.example.springredissonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedissonExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
